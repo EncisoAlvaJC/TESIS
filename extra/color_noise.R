e233142f62b90db8844bc8b66164e3b479b5bb1f
EncisoAlvaJC_lap <enciso.alva.jc@gmail.com>
2017-10-13 00:09:36 -0500

    Avances 12 de octubre de 2017, parte 2

--- extra/color_noise.R
+++ extra/color_noise.R
@@ -20,7 +20,7 @@ n.colores = length(col.exp)
 
 # parametros: fr de muestreo (Hz) y duracion de la sennal (s)
 f.muestreo  = 2*(10**4)
-t.sennal    = 1
+t.sennal    = 5
 
 # contenedor de datos
 esp = matrix(0,nrow=n.colores,
@@ -43,10 +43,16 @@ for(k in 1:n.colores){
 # vector con las frecuencias
 frec.v = espect$freq
 
+# parche
 # este paso equivale a usar el espectro normalizado
 for(k in 1:n.colores){
-  esp[k,] = esp[k,] - esp[k,1]
+  esp[k,] = esp[k,] - esp[k,20]
 }
+
+# este paso equivale a usar el espectro normalizado
+#for(k in 1:n.colores){
+#  esp[k,] = esp[k,] - esp[k,1]
+#}
 
 
 #######################################
@@ -57,19 +63,21 @@ mmax = max(esp)
 mmin = min(esp)
 
 # grafico vacio
+par(mgp=c(2,.5,0))
 plot(0,type='n',
      xlim=c(1,log(max(frec.v),10)),ylim=c(mmin,mmax),
-     xlab = 'Frecuencia (Hz)',
-     ylab='Densidad de Potencia Espectral (dB)',
-     main='Ruidos de color',
-     xaxt='n',las=2)
-axis(1,at=0:10,label=10**(0:10))
+     xlab = 'Frecuency (Hz)',
+     ylab='Power Spectral Density (dB)',
+     main='The Colors of Noise',
+     xaxt='n',las=2,
+     tck=.03)
+axis(1,at=0:10,label=10**(0:10),tck=F)
 
 # marcas logaritmicas de ejes
-axis(1,at=log((1:10)*(10**0),10),label=F)
-axis(1,at=log((1:10)*(10**1),10),label=F)
-axis(1,at=log((1:10)*(10**2),10),label=F)
-axis(1,at=log((1:10)*(10**3),10),label=F)
+axis(1,at=log((1:10)*(10**0),10),label=F,tck=.03)
+axis(1,at=log((1:10)*(10**1),10),label=F,tck=.03)
+axis(1,at=log((1:10)*(10**2),10),label=F,tck=.03)
+axis(1,at=log((1:10)*(10**3),10),label=F,tck=.03)
 
 # lineas per se de los espectros
 for(k in 1:n.colores){
@@ -84,22 +92,35 @@ require(tidyr)
 require(scales)
 
 # arreglo de los datos
-spec.d = data.frame(blanco  =esp[1,],
-                    rosa    =esp[2,],
-                    rojo    =esp[3,],
-                    azul    =esp[4,],
-                    violeta =esp[5,],
-                    Frecuencia = frec.v)
+spec.d = data.frame(White   =esp[1,],
+                    Pink    =esp[2,],
+                    Red     =esp[3,],
+                    Blue    =esp[4,],
+                    Violet  =esp[5,],
+                    Frecuency = frec.v)
 
 # parche: evita error cuando se escala en log la frecuencia,
 # y por estetica para evitar el efecto de puntos no-equiespaciados
-spec.2 = subset(spec.d,Frecuencia>10)
+spec.2 = subset(spec.d,Frecuency>5)
+
+# parche: para escala logaritmica
+esc_log = c()
+for(i in 1:3){
+  esc_log = c(esc_log,c(1,2,3,4,5,6,7,8,9)*10**i)
+}
 
 spec.2 %>%
-  gather(Color,Espectro, blanco,rosa,rojo,azul,violeta) %>%
-  ggplot(aes(x=Frecuencia, y=Espectro, colour=Color)) +
+  gather(Noisecolour,Power, White,Pink,Red,Blue,Violet) %>%
+  ggplot(aes(x=Frecuency, y=Power, colour=Noisecolour)) +
   geom_line() +
+  scale_y_continuous(breaks = c((-20/5):(15/5))*5,
+                     labels = waiver())+
   scale_x_continuous(trans = log10_trans(),
-                     breaks = trans_breaks("log10", function(x) 10^x),
+                     #breaks = trans_breaks("log10", function(x) 10^x),
+                     breaks = c(10,10**2,10**3,10**4,10**5),
                      labels = trans_format("log10", math_format(10^.x)))+
-  scale_color_manual(values=c('blue','black','red','pink','violet'))
+  #scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
+  #              labels = trans_format("log10", math_format(10^.x))) +
+  scale_color_manual(values=c('blue','pink','red','violet','black'))+
+annotation_logticks(sides = 'b')  
+
