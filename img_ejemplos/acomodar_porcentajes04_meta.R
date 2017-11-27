3563f7224e998b1c69ddc2f02ffcd760ffd24dd4
EncisoAlvaJC_lap <enciso.alva.jc@gmail.com>
2017-11-27 02:57:49 -0600

    Avances Ya esta la metodologia bien y parte de los resultados

--- img_ejemplos/acomodar_porcentajes04_meta.R
+++ img_ejemplos/acomodar_porcentajes04_meta.R
@@ -3,7 +3,7 @@
 data_dir    = '~/TESIS/graf_datos/estacionariedad_sf/'
 central_dir = '~/TESIS/TESIS/img_art_dfa'
 info_dir    = '~/TESIS/TESIS/articulo_dfa'
-g_dir       = central_dir
+g_dir       = '~/TESIS/TESIS/img_art_dfa'
 setwd(central_dir)
 
 dur_chunk = 30
@@ -14,118 +14,65 @@ guardar_archivo = F
 nombre_archivo  = paste0('porcentajes_',toString(dur_chunk),'.xlsx')
 
 ###############################################################################
-# datos generales
+# librerias
+require('readxl')
+require('xlsx')
 
+require('ggplot2')
+require('ggpubr')
 
-nomb_dir  = c('VCNNS',
-              'MJNNVIGILOScCanal',
-              'JANASUE_revisado',
-              'GH',
-              'GURM_revisado',
-              'CLMN10SUE',
-              'RLMN',
-              'RRMNS_2',
-              'JGMN6SUE',
-              'FGH_EEGdescompuesto',
-              'MGNA',
-              'EMNN')
-nomb_arch = c('VCNNS1',
-              'MJNNVIGILOS',
-              'JANASUE',
-              'GH24031950SUE?O',
-              'GURM251148SUE',
-              'CLMN10SUE',
-              'RLMN10SUE',
-              'RRMNS',
-              'JGMN6SUE',
-              'FGHSUE',
-              'MGNA5SUE',
-              'EMNNS')
-nomb_facil = c('VCR',
-               'MJH',
-               'JAE',
-               'GHA',
-               'MFGR',
-               'CLO',
-               'RLO',
-               'RRU',
-               'JGZ', 
-               'FGH',
-               'MGG',
-               'EMT')
-channel   = c('C3','C4','CZ',
-              'F3','F4','F7','F8',
-              'FP1','FP2','FZ',
-              'O1','O2','P3','P4','PZ',
-              'T3','T4','T5','T6',
-              'LOG','ROG',
-              'EMG')
-frecuenciasss = c(200,
-                  512,512,
-                  200,
-                  200,#segun valeria GUR=200 Hz
-                  #512, #segun la libreta GURM=512
-                  512,512,
-                  200,#solo tiene 3 horas
-                  512,
-                  512,512,
-                  200)
-grupo_de = c(0,0,0,0,0,1,1,1,1,-1,-1,-1)
-#grupo_de = c(-1,0,0,-1,-1,1,1,-1,-1,-1,-1,-1)
-#grupo_de =  c(0,-1,-1,0,0,-1,-1,1,1,-1,-1,-1)
-#grupo_de = c(1,1,1,1,1,1,1,1,1,-1,0,0)
+require('Rmisc')
 
 ###############################################################################
-# para grabar en formato de excel
-require(xlsx)
+# datos generales
+info     = read_excel(paste0(info_dir,'/info_tecnico.xlsx'))
 
-# archivo contenedor
-matriz = matrix(nrow=2,ncol=2)
-matriz[1,1]=0
-matriz[1,2]=0
-matriz[2,1]=0
-matriz[2,2]=0
-rownames(matriz)=NULL
-colnames(matriz)=NULL
-#write.xlsx(matriz,file='porcentajes')
+kanales  = read_excel(paste0(info_dir,'/info_canales.xlsx'))
+if(orden_stam){
+  kanales  = read_excel(paste0(dir_info,'/info_canales_alterno.xlsx'))
+}
+n.canales    = length(kanales$Etiqueta)
+canales.arch = kanales$Nombre_archivo
 
+n.participantes = length(info$Nombre)
+
+###############################################################################
 # contenedores de datos
-PORCE_MOR  = matrix(nrow=22,ncol=12)
-PORCE_NMOR = matrix(nrow=22,ncol=12)
-PORCE_TOT  = matrix(nrow=22,ncol=12)
+PORCE_MOR  = matrix(nrow=n.canales,ncol=n.participantes)
+PORCE_NMOR = matrix(nrow=n.canales,ncol=n.participantes)
+PORCE_TOT  = matrix(nrow=n.canales,ncol=n.participantes)
 
-TOTAL_MOR  = matrix(nrow=23,ncol=12)
-TOTAL_NMOR = matrix(nrow=23,ncol=12)
-TOTAL_TOT  = matrix(nrow=23,ncol=12)
+TOTAL_MOR  = matrix(nrow=n.canales+1,ncol=n.participantes)
+TOTAL_NMOR = matrix(nrow=n.canales+1,ncol=n.participantes)
+TOTAL_TOT  = matrix(nrow=n.canales+1,ncol=n.participantes)
 
-colnames(PORCE_MOR)  = nomb_facil
-colnames(PORCE_NMOR) = nomb_facil
-colnames(PORCE_TOT)  = nomb_facil
+colnames(PORCE_MOR)  = info$Nombre
+colnames(PORCE_NMOR) = info$Nombre
+colnames(PORCE_TOT)  = info$Nombre
 
-row.names(PORCE_MOR)  = channel
-row.names(PORCE_NMOR) = channel
-row.names(PORCE_TOT)  = channel
+row.names(PORCE_MOR)  = kanales$Etiqueta
+row.names(PORCE_NMOR) = kanales$Etiqueta
+row.names(PORCE_TOT)  = kanales$Etiqueta
 
-colnames(TOTAL_MOR)  = nomb_facil
-colnames(TOTAL_NMOR) = nomb_facil
-colnames(TOTAL_TOT)  = nomb_facil
+colnames(TOTAL_MOR)  = info$Nombre
+colnames(TOTAL_NMOR) = info$Nombre
+colnames(TOTAL_TOT)  = info$Nombre
 
-row.names(TOTAL_MOR)  = c(channel,'Total')
-row.names(TOTAL_NMOR) = c(channel,'Total')
-row.names(TOTAL_TOT)  = c(channel,'Total')
+row.names(TOTAL_MOR)  = c(kanales$Etiqueta,'Total')
+row.names(TOTAL_NMOR) = c(kanales$Etiqueta,'Total')
+row.names(TOTAL_TOT)  = c(kanales$Etiqueta,'Total')
 
 ###############################################################################
 # ejecucion per se
-for(sujeto in 1:12){
-  source('~/TESIS/TESIS/img_ejemplos/acomodar_porcentajes02.R')
+for(sujeto in 1:n.participantes){
+  source('~/TESIS/TESIS/img_ejemplos/acomodar_porcentajes04.R')
 }
 
 if(guardar_archivo){
   setwd(g_dir)
-  write.xlsx(matriz,file=nombre_archivo)
-  
+
   write.xlsx(TOTAL_MOR,sheetName ='total_MOR',
-             file=nombre_archivo,append=T)
+             file=nombre_archivo,append=F)
   write.xlsx(TOTAL_NMOR,sheetName='total_NMOR',
              file=nombre_archivo,append=T)
   write.xlsx(TOTAL_TOT,sheetName ='total_TODO',
